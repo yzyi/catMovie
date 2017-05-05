eacd9310d52717976feb453bea615d72dde3930a
wfli <wfli888@163.com>
2017-05-05 21:54:46 +0800

    第一次提交

--- src/components/Find.vue
+++ src/components/Find.vue
@@ -1,5 +1,9 @@
 <template lang="html">
-	<h1>找资讯</h1>
+	<div class="head">
+		<div class="head-inner">
+			找资讯
+		</div>
+	</div>
 </template>
 <script>
 	export default{
@@ -7,5 +11,29 @@
 	}
 </script>
 <style lang="css">
+*{padding:0;margin:0;}
+html{
+	font-size:62.5%;
+}
+.head{
+	width: 100%;
+	height:10.0rem;
+	background:red;
+	display:flex;
+	justify-content:center;
+	align-items: center;
+}
+	
+.head-inner{
+	width:90%;
+	height:6.0rem;
+	background:white;
+	color:#a9a9a9;
+	font-size:3rem;
+	border-radius:4.0rem;
+	text-align: center;
+	line-height:6rem; 
+}
 	
 </style>
+	
