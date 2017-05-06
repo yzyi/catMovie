de301fb78398cda031faf6140956597681f9a7d7
lucy <553015559@qq.com>
2017-05-06 22:06:22 +0800

    热映页上video图片更新

--- src/components/Hot.vue
+++ src/components/Hot.vue
@@ -4,29 +4,28 @@
 			<h1>热映</h1>
 		</div>
 		<section id="hot-showing">
-			<div class="hot">
-				<ul class="show" v-for="i in hotData">
-					<li>
-						<video width="50" height="100" controls>
-							<source :src="i.url" type="video/mp4">
-						</video>
-					</li>
-					<li>
-						<p>{{i.movieName}}</p>
-						<h4><span>观众</span>{{i.sc}}</h4>
-						<p>{{i.name}}</p>
-						<p>{{i.originName}}</p>
+			<ul class="show" v-for="i in hotData">
+				<li class="hot">
+					<video controls class="video">
+						<source type="video/mp4">
+					</video>
+					<img :src="i.img" class="poster">
+				</li>
+				<li>
+					<h3>{{i.movieName}}</h3>
+					<h4><span>观众</span>{{i.sc}}</h4>
+					<p>{{i.name}}</p>
+					<p>{{i.originName}}</p>
 
-						<router-link to="/buy" class="buy">购买</router-link>
-					</li>
-				</ul>
-				<div class="headLine" v-for="x in hotData.newsHeadlines">
+					<router-link to="/buy" class="buy">购买</router-link>
+				</li>
+				<li class="headLine" v-for="x in hotData.newsHeadlines">
 					<router-link to="/buy">
-						<i>{{x.type}}</i>
-						<span>{{x.title}}</span>
-					</router-link>
-				</div>
-			</div>
+					<i>{{x.type}}</i>
+					<span>{{x.title}}</span>
+				</router-link>
+				</li>
+			</ul>
 		</section>
 	</div>
 </template>
@@ -45,11 +44,47 @@
 			// 在这里做http数据请求
 			this.$http.get("http://duif.applinzi.com/video.php").then(function(res){
 				this.hotData = JSON.parse(res.bodyText).data;
-				console.log(JSON.parse(res.bodyText));
+				console.log(JSON.parse(res.bodyText).data);
 			})
 		}
 	}
 </script>
 <style lang="css">
-	
+	li{
+		list-style: none;
+	}
+	h4,i,b,h1,h2,h3,h5,h6{
+		font-weight: normal;
+		font-style: normal;
+	}
+	html{
+		font-size:1rem;
+	}
+	#hot-showing{
+		padding-left:1.0rem;
+		width: 100%;
+		font-size: 14px;
+	}
+	.show{
+		width: 100%;
+		border-bottom: 1px solid #eee;
+		display: flex;
+	}
+	.hot{
+		width: 30%;
+		height: 12rem;
+		padding:1.0rem; 
+		position: relative;
+	}
+	.video{
+		width: 1rem;
+		height: 1rem;
+		position: absolute;
+		left: 37%;
+		top:45%;
+	}
+	.poster{
+		width: 100%;
+		height: 100%;
+	}
 </style>
--- src/components/Wait.vue
+++ src/components/Wait.vue
@@ -1,6 +1,24 @@
 <template lang="html">
 	<div>
 		<h1>待映</h1>
+		<section id="hot-showing">
+			<ul class="hot-show" v-for="i in hotData">
+				<li class="hot-image-text">
+					<video controls class="hot-video">
+						<source type="video/mp4">
+					</video>
+					<img :src="i.img" class="hot-poster">
+					<div class="play-icon"></div>
+				</li>
+				<li class="hot-info">
+					<h3>{{i.movieName}}</h3>
+					<h4><span>观众</span>{{i.sc}}</h4>
+					<p>{{i.name}}</p>
+					<p>{{i.originName}}</p>
+					<router-link to="/buy" class="buy">购买</router-link>
+				</li>
+			</ul>
+		</section>
 	</div>
 </template>
 
@@ -11,14 +29,83 @@
 	export default{
 		data (){
 			return {
-				
+				hotData : []
 			}
 		},
 		mounted (){
 			// 在这里做http数据请求
+			this.$http.get("http://duif.applinzi.com/video.php").then(function(res){
+				this.hotData = JSON.parse(res.bodyText).data;
+				console.log(JSON.parse(res.bodyText).data);
+			})
 		}
 	}
 </script>
 <style lang="css">
-	
+	li{
+		list-style: none;
+	}
+	h4,i,b,h1,h2,h3,h5,h6{
+		font-weight: normal;
+		font-style: normal;
+	}
+	html{
+		font-size:1rem;
+	}
+	#hot-showing{
+		padding-left:1.0rem;
+		width: 100%;
+		font-size: 14px;
+	}
+	.hot-show{
+		width: 100%;
+		border-bottom: 1px solid #eee;
+		display: flex;
+	}
+	.hot-image-text{
+		width: 25%;
+		height: 11rem;
+		padding:1.0rem; 
+		position: relative;
+	}
+
+	.hot-video{
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		left: 0;
+		top:0;
+		opacity: 0;
+	}
+	.hot-poster{
+		width: 100%;
+		height: 100%;
+	}
+	.play-icon{
+		width: 38px;
+		height: 38px;
+		border:1px solid #fff;
+		background: rgba(0,0,0,.4);
+		border-radius: 50%;
+		position: absolute;
+		left:39%;
+		top:43%;
+	}
+	.play-icon::before{
+		content: "";
+		width: 0;
+		height: 0;
+		display: block;
+		border:10px solid #fff;
+		border-top:10px solid rgba(0,0,0,0);
+		border-right:10px solid rgba(0,0,0,0);
+		border-bottom:10px solid rgba(0,0,0,0);
+		position: absolute;
+		left: 41%;
+		top:25%;
+	}
+	.hot-info{
+		height: 11rem;
+		padding:1.0rem; 
+	}
 </style>
