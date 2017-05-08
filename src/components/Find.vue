53ba3ef67f9d43abb1c76f460dc9edf3d5e838c4
wfli <wfli888@163.com>
2017-05-08 13:29:04 +0800

    find静态页面完成

--- src/components/Find.vue
+++ src/components/Find.vue
@@ -3,7 +3,7 @@
 		<router-view></router-view>
 		<div class="head">
 		<div class="head-inner">
-			找资讯
+			<img src="../assets/search.png" alt="" class="search">找资讯
 		</div>
 	</div>
 	<div class="f-mune">
@@ -12,17 +12,33 @@
 		<router-link to="/Mall" class="f-munes munesthired">周边商城</router-link>
 		<router-link to="/Real" class="f-munes muneslast">实时票房</router-link>
 	</div>
-	<ul>
-		<li v-for="x in itemData">
-			<p>{{x.title}}</p>
+	<ul class="mainList">
+		<li v-for="x in itemData" class="items">
+			<p class="items-title">{{x.title}}</p>
+			<div class="items-img">
+				<img src="../assets/tu1.png" alt="" class="items-imgs">
+				<img src="../assets/tu2.png"" alt="" class="items-imgs">
+				<img src="../assets/tu3.png"" alt="" class="items-imgs">
+			</div>
+			<div class="items_bom">
+				<div class="bom-left">
+					<span class="nickName">{{x.user.nickName}}</span>
+					<span class="time">54分钟前</span>
+				</div>
+				<div class="bom-right">
+					<span class="viewCount"><img src="../assets/tu6.png" alt="">{{x.viewCount}}</span>
+					<span><img src="../assets/tu5.png" alt="">{{x.commentCount}}</span>
+				</div>
+				
+			</div>
 			
 			<!-- <img :src="x.url" alt=""> -->
-
-
 		</li>
 	</ul>
 	</div>
 </template>
+
+
 <script>
 	import Vue from "vue"
 	import VueResource from "vue-resource" 
@@ -39,8 +55,8 @@
 				// this.itemData = JSON.parse(res.bodyText).data.feeds;
 				this.itemData = res.body.data.feeds
 				// console.log(res.bodyText);
-				console.log(JSON.parse(res.bodyText));
-				console.log(res.body);
+				// console.log(JSON.parse(res.bodyText));
+				// console.log(res.body);
 			})
 		}
 	}
@@ -54,6 +70,10 @@ res.body 取出来的是对象 就可以直接点属性，取出对应的数据
 </script>
 <style lang="css">
 *{padding:0;margin:0;}
+ul {
+	list-style: none;
+
+}
 html{
 	font-size:62.5%;
 }
@@ -68,13 +88,19 @@ html{
 .head-inner{
 	width:90%;
 	height:6.0rem;
-	background:white;
 	color:#a9a9a9;
 	font-size:3rem;
 	border-radius:4.0rem;
 	text-align: center;
-	line-height:6rem; 
+	line-height:6rem;
+	background:url(../assets/search.png) no-repeat; 
+	background:white;
 }
+.search{
+	margin-right:0.5rem;
+
+}
+
 .f-mune{
 	display: flex;
 	width: 100%;
@@ -90,8 +116,96 @@ html{
 	justify-content: center;
 	background:url(../assets/1.png) no-repeat;
 	background-position:center 4rem;
+	text-decoration: none;
+	font-size:1.5rem;
+	color:#656565;
+}
+.muneslast{
+	background:url(../assets/tu9.png) no-repeat;
+	background-position:center 4rem;
+}
+.munesfirst{
+	background:url(../assets/1.png) no-repeat;
+	background-position:center 4rem;
+}
+.munestwo{
+	background:url(../assets/tu7.png) no-repeat;
+	background-position:center 4rem;
+}
+.munesthired{
+	background:url(../assets/tu8.png) no-repeat;
+	background-position:center 4rem;
+}
+
+/*mian 列表项部分*/
+/*ul*/
+.mainList{
+	width: 100%;
+}
+/*li 高度不一样高，不设置高*/
+.items{
+	width: 90%;
+	margin:0 auto;
+	border-bottom:0.1rem solid #f7f7f7;
+}
+
+/*p title*/
+.items-title{
+	width: 100%;
+	line-height:7.3rem;
+	font-size: 3.0rem;
+	text-align:left;
+	height: 7.3rem;
+}
+
+/*放图片的div*/
+.items-img{
+	width: 100%;
+	height: 160px;
+}
+	
+/*图片*/
+.items-imgs{
+	float:left;
+	width: 32.5%;
+	margin-right:9px;
+}
+.items-imgs:last-of-type{
+	margin-right:0;
+}
+/*底部*/
+.items_bom{
+	width: 100%;
+	height:5.4rem;
+	margin-top: 2.7rem;
+	font-size:2.2rem;
+	padding-top:3rem;
+	color:#989898;
+}
+	
+/*左边*/
+.bom-left{
+	width: 230px;
+	float:left;
 
 }
+.nickName{
+	margin-right:1.6rem;
+}
+.bom-right{
+	width: 17.2rem;
+	float: right;
+	
+}
+.viewCount{
+	margin-right: 2.6rem;
+}
+.bom-right img{
+	margin-right:0.7rem;
+}
+
+
+
 	
 	
 	
