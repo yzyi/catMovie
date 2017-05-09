c73ebf7063357b4c0fd18d73a9589cefe430d40a
wzhen <1328380414@qq.com>
2017-05-09 19:43:47 +0800

    首页-找片

--- src/components/Address.vue
+++ src/components/Address.vue
@@ -9,14 +9,7 @@
 	import VueResource from "vue-resource"
 	Vue.use(VueResource)
 	export default{
-		data (){
-			return {
-				
-			}
-		},
-		mounted (){
-			// 在这里做http数据请求
-		}
+		
 	}
 </script>
 <style lang="css">
